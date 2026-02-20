c8eb2f7305dbcbe4ea0fb9bf2e8913a9cde9761f
Alexander Hück <hueck.alexander@gmail.com>
2026-02-20 10:04:14 +0100

    Combos

--- config.h
+++ config.h
@@ -46,5 +46,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define CHORDAL_HOLD
 #define PERMISSIVE_HOLD
 #define SPECULATIVE_HOLD
+
 #define EXTRA_SHORT_COMBOS
 #define COMBO_ONLY_FROM_LAYER 0
+#define COMBO_TERM 40
+#define COMBO_STRICT_TIMER
--- keymap.c
+++ keymap.c
@@ -243,9 +243,18 @@ const char PROGMEM chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] =
 // clang-format on
 
 enum combos {
-  ER_ESC,
+  DF_ESC,
+  JK_TAB,
+  FJ_ENTER,
+  UI_LINE,
 };
-const uint16_t PROGMEM esc_combo[] = {KC_E, KC_R, COMBO_END};
-combo_t key_combos[]               = {
-    [ER_ESC] = COMBO(esc_combo, KC_ESC),
+const uint16_t PROGMEM esc_combo[]   = {HOME_D, HOME_F, COMBO_END};
+const uint16_t PROGMEM tab_combo[]   = {HOME_J, HOME_K, COMBO_END};
+const uint16_t PROGMEM enter_combo[] = {HOME_F, HOME_J, COMBO_END};
+const uint16_t PROGMEM line_combo[]  = {KC_U, KC_I, COMBO_END};
+combo_t key_combos[]                 = {
+    [DF_ESC]   = COMBO(esc_combo, KC_ESC),
+    [JK_TAB]   = COMBO(tab_combo, KC_TAB),
+    [FJ_ENTER] = COMBO(enter_combo, KC_ENTER),
+    [UI_LINE]  = COMBO(line_combo, SELLINE),
 };
